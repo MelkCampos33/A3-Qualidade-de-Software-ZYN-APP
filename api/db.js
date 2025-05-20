34788611f4b8c3b36df0adb8aee3c4ad75c10f9e
Melquisedeque F. Campos <108761793+MelkCampos33@users.noreply.github.com>
2025-05-20 14:55:08 -0300

    redirecionando pasta da API do backend

---
+++ api/db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'zyn_app_db',
+  port: 3306
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+    return;
+  }
+  console.log('Conectado ao banco de dados MySQL!');
+  connection.release();
+});
+
+
+module.exports = pool.promise();
